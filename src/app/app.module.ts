ec04fce55215b349ed2a1fc0cbe388f2f9aa5b6a
Rabindra Patra <rabindrapatra@fico.com>
2021-04-18 10:49:04 +0530

    added ComponentHello as bootstrap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentHelloComponent } from './components/component-hello/component-hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentHelloComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ComponentHelloComponent]
 })
 export class AppModule { }
---
+++ src/app/components/component-hello/component-hello.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-hello',
+  templateUrl: './component-hello.component.html',
+  styleUrls: ['./component-hello.component.css']
+})
+export class ComponentHelloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
